69e6d71b25504ae3033ba45e71e9a21c4d3efdf9
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-04-02 01:00:35 +0200

    data service getUser

--- ng7/src/app/home/home.component.ts
+++ ng7/src/app/home/home.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
   h1Style: boolean = false;
 
-  constructor() { }
+  users: Object;
+
+  constructor(private data: DataService) {}
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
 
-  firstClick(){
+  firstClick() {
     this.h1Style = true;
+    this.data.firstClick();
   }
-
 }
